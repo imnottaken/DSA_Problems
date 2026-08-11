08410b6d4f73fb2f0e2d2319981cc54a727586df
Koustav Dey <72761561+imnottaken@users.noreply.github.com>
2026-08-11 23:10:51 +0530

    Time: 10 ms (69.97%) | Memory: 35.1 MB (54.92%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+
+        int left = *max_element(weights.begin(), weights.end());
+        int right = accumulate(weights.begin(), weights.end(), 0);
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            int daysNeeded = 1;
+            int currentWeight = 0;
+
+            for (int weight : weights) {
+
+                if (currentWeight + weight > mid) {
+                    daysNeeded++;
+                    currentWeight = 0;
+                }
+
+                currentWeight += weight;
+            }
+
+            if (daysNeeded <= days) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+};
