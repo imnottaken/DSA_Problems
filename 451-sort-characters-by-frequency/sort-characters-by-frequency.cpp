cb43285a9a25db9a7ca89ff8f5f751a589348ee6
Koustav Dey <72761561+imnottaken@users.noreply.github.com>
2026-08-13 19:06:40 +0530

    Time: 8 ms (11.99%) | Memory: 10.5 MB (92.75%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -3,22 +3,21 @@ public:
     string frequencySort(string s) {
         int freq[256] = {0};
 
-        for(char c : s) {
+        for(char c: s){
             freq[c]++;
         }
 
         string ans = "";
 
-        for(int count = s.size(); count > 0; count--) {
-            for(char c = 0; c < 127; c++) {
-                if(freq[c] == count) {
-                    for(int j = 0; j < count; j++) {
+        for(int count = s.size(); count > 0; count--){
+            for(char c = 0; c < 127; c++){
+                if(freq[c] == count){
+                    for(int j = 0; j < count; j++){
                         ans += c;
                     }
                 }
             }
         }
-
-        return ans;
+        return ans; 
     }
 };
